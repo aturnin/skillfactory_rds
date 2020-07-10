1bc11f347fc90fba1d64818644623de5dec4a8f2
aturnin <gterrans@gmail.com>
2020-07-10 14:25:24 +0300

    Init module_EDA

---
+++ module_2/dict_cat.py
@@ -0,0 +1,37 @@
+"""
+
+"""
+
+
+dict_cat = {    
+        'school': ['GP', 'MS'],
+        'sex': ['M', 'F'],
+        'age': ['numerical'],
+        'address': ['U', 'R'],
+        'famsize': ['GT3', 'LE3'],
+        'Pstatus': ['T', 'A'],
+        'Medu': [0, 1, 2, 3, 4],
+        'Fedu': [0, 1, 2, 3, 4],
+        'Mjob': ['teacher', 'health', 'services', 'at_home', 'other'],
+        'Fjob': ['teacher', 'health', 'services', 'at_home', 'other'],
+        'reason': ['home', 'reputation', 'course', 'other'],
+        'guardian': ['mother', 'father', 'other'],
+        'traveltime': [1, 2, 3, 4],
+        'studytime': [1, 2, 3, 4],
+        'failures': [0, 1, 2, 3],
+        'schoolsup': ['yes', 'no'],
+        'famsup': ['yes', 'no'],
+        'paid': ['yes', 'no'],
+        'activities': ['yes', 'no'],
+        'nursery': ['yes', 'no'],
+        'granular': [-12, -9, -6, -3],
+        'higher': ['yes', 'no'],
+        'internet': ['yes', 'no'],
+        'romantic': ['yes', 'no'],
+        'famrel': [1, 2, 3, 4, 5],
+        'freetime': [1, 2, 3, 4, 5],
+        'goout':  [1, 2, 3, 4, 5],
+        'health': [1, 2, 3, 4, 5],
+        'absences': ['numerical'],
+        'score': ['numerical']       
+        }
